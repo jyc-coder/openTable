95e93f7cc301385bc5fdf2fe32df74f9cc4a7b76
jyc-coder <jyc4648@gmail.com>
2023-07-20 01:37:16 +0900

    feat: implement inquire available Times
    inquire available Times after setting partySize, Date, Time options and Click the "Find a Time" button

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,21 +1,43 @@
 'use client'
 import React, { useState } from 'react'
-import { partySize, times } from '../../../../data'
+import { partySize as partySizes, times } from '../../../../data'
 import DatePicker from 'react-datepicker'
+import useAvailabilities from '@/hooks/useAvailabilities'
+import { CircularProgress } from '@mui/material'
+import Link from 'next/link'
+import { Time, convertToDisplayTime } from '@/utils/convertToDisplayTime'
 
 export default function ReservationCard({
   openTime,
   closeTime,
+  slug,
 }: {
   openTime: string
   closeTime: string
+  slug: string
 }) {
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date())
+  const { data, loading, error, fetchAvailabilities } = useAvailabilities()
+  const [time, setTime] = useState(openTime)
+  const [partySize, setPartySize] = useState('2')
+  const [day, setDay] = useState(new Date().toISOString().split('T')[0])
   const handleChangeDate = (date: Date | null) => {
     if (date) {
-      return setSelectedDate(date)
+      const offset = date.getTimezoneOffset() * 60000
+      const localDate = new Date(date.getTime() - offset)
+      setDay(localDate.toISOString().split('T')[0])
+      return setSelectedDate(localDate)
     }
     return setSelectedDate(null)
+  }
+
+  const handleClick = () => {
+    fetchAvailabilities({
+      slug,
+      day,
+      time,
+      partySize,
+    })
   }
 
   const filterTimesByRestaurantOpenWindow = () => {
@@ -37,12 +59,18 @@ export default function ReservationCard({
   return (
     <div className="fixed w-[15%] bg-white rounded p-3 shadow">
       <div className="pb-2 font-bold text-center border-b">
-        <h4 className="text-lg mr-7">Make a Reservation</h4>
+        <h4 className="text-lg mr-7">Make a Reservation {time}</h4>
       </div>
       <div className="flex flex-col my-3">
         <label htmlFor="">Party size</label>
-        <select name="" className="py-3 font-light border-b" id="">
-          {partySize.map((size) => (
+        <select
+          name=""
+          className="py-3 font-light border-b"
+          id=""
+          value={partySize}
+          onChange={(e) => setPartySize(e.target.value)}
+        >
+          {partySizes.map((size) => (
             <option value={size.value}>{size.label}</option>
           ))}
         </select>
@@ -60,7 +88,13 @@ export default function ReservationCard({
         </div>
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Time</label>
-          <select name="" id="" className="py-3 font-light border-b">
+          <select
+            name=""
+            id=""
+            className="py-3 font-light border-b"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          >
             {filterTimesByRestaurantOpenWindow().map((time) => (
               <option value={time.time}>{time.displayTime}</option>
             ))}
@@ -68,10 +102,33 @@ export default function ReservationCard({
         </div>
       </div>
       <div className="mt-5">
-        <button className="w-full h-16 px-4 font-bold text-white bg-red-600 rounded">
-          Find a Time
+        <button
+          className="w-full h-16 px-4 font-bold text-white bg-red-600 rounded"
+          onClick={handleClick}
+          disabled={loading}
+        >
+          {loading ? <CircularProgress color="inherit" /> : 'Find a Time'}
         </button>
       </div>
+      {data && data.length ? (
+        <div className="mt-4">
+          <p className="text-reg">Select a Time</p>
+          <div className="flex flex-wrap mt-2">
+            {data.map((time) => {
+              return time.available ? (
+                <Link
+                  href={`/reserve/${slug}?date=${day}T${time.time}&partySize=${partySize}`}
+                  className="w-24 p-2 mb-3 mr-3 text-center text-white bg-red-600 rounded cursor-pointer"
+                >
+                  <p className="text-sm font-bold">{convertToDisplayTime(time.time as Time)}</p>
+                </Link>
+              ) : (
+                <p className="w-24 p-2 mb-3 mr-3 bg-gray-300 rounded"></p>
+              )
+            })}
+          </div>
+        </div>
+      ) : null}
     </div>
   )
 }
